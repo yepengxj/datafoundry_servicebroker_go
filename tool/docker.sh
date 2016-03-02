496562493cf6afd8f0ed8472c9523980110338bb
HeHongling <hank.sunday@gmail.com>
2016-03-02 22:30:43 +0800

    Revert "Delete docker.sh"
    This reverts commit 285353a9d908232e9847c28b654a5e90ed7258f4.

---
+++ tool/docker.sh
@@ -0,0 +1,11 @@
+docker build -t mongodb_aws .
+
+docker run -d -p 8000:8000 \
+	-e "ETCDENDPOINT=http://192.168.99.100:2379"  \
+	-e "BROKERPORT=8000"	 \
+	-e "MONGOURL=54.222.155.67:27017"	 \
+	-e "MONGOADMINUSER=asiainfoLDP"  \
+	-e "MONGOADMINPASSWORD=6ED9BA74-75FD-4D1B-8916-842CB936AC1A"  \
+	-e "AWS_ACCESS_KEY_ID=AKIAO2SO52RKIE7BCSHA"
+	-e "AWS_SECRET_ACCESS_KEY=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"
+	--name mongodb_aws mongodb_aws
